320ff0c11849189e03c60f54cabb593c15764a8c
tien <tienqnguyen15@gmail.com>
2023-03-13 10:28:24 +0700

    config flyway and create flyway script for student table

--- src/main/java/com/example/book/Model/Student.java
+++ src/main/java/com/example/book/Model/Student.java
@@ -2,15 +2,21 @@ package com.example.book.Model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
+@Table(name="Students")
 public class Student {
     @Id
     private Long id;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="age")
     private Integer age;
 }
---
+++ src/main/resources/db/migration/V2__Insert_student_tien.sql
@@ -0,0 +1,3 @@
+
+
+INSERT INTO [dbo].[students] (id, first_name, last_name, age) values (1, 'tien', 'nguyen', 28)
