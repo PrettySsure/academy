efb8d6c71bd763da18ccb10ac9237d19d7c9a538
tien <tienqnguyen15@gmail.com>
2023-03-26 15:34:40 +0700

    config flyway and create sql script to create students table

---
+++ src/main/resources/db/migration/V1__Create_students_table.sql
@@ -0,0 +1,19 @@
+BEGIN TRANSACTION;
+
+IF(
+      NOT EXISTS (
+      select * from INFORMATION_SCHEMA.TABLES
+      where TABLE_SCHEMA='dbo' AND TABLE_NAME='Students'
+      )
+  )
+CREATE TABLE [dbo].[Students] (
+    id int NOT NULL PRIMARY KEY,
+    first_name varchar(20),
+    last_name varchar(50),
+    age int
+                              );
+
+
+
+
+COMMIT TRANSACTION;
